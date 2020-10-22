c81e615f97afe1257bf4520694d42d11461bf4d4
avish <avishek.bhattacharjee@hotmail.com>
2020-10-22 08:28:28 -0700

    first commit

---
+++ employee-service-docker/src/main/java/com/ab/EmployeeServiceDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceDockerApplication.class, args);
+	}
+
+}
